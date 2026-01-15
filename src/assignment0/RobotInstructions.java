aa3de12680d1514cceff93d2ecfc47e749425fb3
ClaraR-Blip <c.r.ripin@wustl.edu>
2026-01-14 19:08:52 -0600

    hi

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,22 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(70);
+		robot.turnRight(135);
+		robot.forward(120);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(120);
+		robot.turnRight(135);
+		robot.forward(70);
+		robot.turnLeft(90);
+		robot.forward(120);
+
+
+
+
+
 	}
 
 }
